2b445cadd6618fcdc6e1d692b629a7da20e34cda
BonyBabu <37437260+BonyBabu@users.noreply.github.com>
2026-02-18 00:31:31 +1100

    Modify VM creation script to use dynamic image names

--- VM/create-vm.sh
+++ VM/create-vm.sh
@@ -1,8 +1,10 @@
+cp noble-server-cloudimg-amd64.img $1.img
+cp seed.iso $1.iso
 qemu-system-x86_64 \
-  -m 4G \
+  -m 1G \
   -accel hvf \
   -cpu host \
-  -drive file=noble-server-cloudimg-amd64.img,format=qcow2 \
-  -drive file=seed.iso,format=raw,index=1,media=cdrom \
+  -drive file=$1.img,format=qcow2 \
+  -drive file=$1.iso,format=raw,index=1,media=cdrom \
   -nographic \
   -serial mon:stdio
