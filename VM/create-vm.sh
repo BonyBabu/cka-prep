fd638e98cb85513ba7e1428ac26b7922fb782935
BonyBabu <37437260+BonyBabu@users.noreply.github.com>
2026-02-18 00:40:41 +1100

    Refactor VM creation script to use instances folder

--- VM/create-vm.sh
+++ VM/create-vm.sh
@@ -1,10 +1,12 @@
-cp noble-server-cloudimg-amd64.img $1.img
-cp seed.iso $1.iso
+vm_folder=instances
+mkdir -p instances
+cp noble-server-cloudimg-amd64.img "$vm_folder/$1.img"
+cp seed.iso "$vm_folder/$1.iso"
 qemu-system-x86_64 \
   -m 1G \
   -accel hvf \
   -cpu host \
-  -drive file=$1.img,format=qcow2 \
-  -drive file=$1.iso,format=raw,index=1,media=cdrom \
+  -drive file="$vm_folder/$1.img",format=qcow2 \
+  -drive file="$vm_folder/$1.iso",format=raw,index=1,media=cdrom \
   -nographic \
   -serial mon:stdio
