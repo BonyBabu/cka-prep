e13fd7070a31eb8acbe1d243b7c0ab2eab26e79d
BonyBabu <37437260+BonyBabu@users.noreply.github.com>
2026-02-18 00:23:56 +1100

    Update VM script to use more memory and new drives

--- VM/create-vm.sh
+++ VM/create-vm.sh
@@ -1,7 +1,8 @@
 qemu-system-x86_64 \
-  -m 1G \
-  -smp 1 \
+  -m 4G \
   -accel hvf \
   -cpu host \
-  -drive file=$1\
-  -nographic
+  -drive file=noble-server-cloudimg-amd64.img,format=qcow2 \
+  -drive file=seed.iso,format=raw,index=1,media=cdrom \
+  -nographic \
+  -serial mon:stdio
