da4a724ac3e07bd1e0543f1293106ba03b28cfcc
BonyBabu <37437260+BonyBabu@users.noreply.github.com>
2026-02-17 23:19:17 +1100

    Refactor VM creation script for improved parameters
    Removed image creation command and updated VM launch options.

--- VM/create-vm.sh
+++ VM/create-vm.sh
@@ -1,13 +1,7 @@
-qemu-img create -f qcow2 $1.qcow2 15G
-
 qemu-system-x86_64 \
--m 1G \
--smp 1 \
--cdrom $2 \
--drive file=$1.qcow2,if=virtio \
--vga virtio \
--display default,show-cursor=on \
--usb \
--device usb-tablet \
--cpu host \
--machine type=q35,accel=hvf \
+  -m 1G \
+  -smp 1 \
+  -accel hvf \
+  -cpu host \
+  -drive file=$1\
+  -nographic
